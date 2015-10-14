a8c0ff829fe3492813c76c4c647f7d2c2fe2907c
Hampus Joakim Nilsson <mail@hjnilsson.com>
2015-10-14 14:42:01 +0200

    Initial project (from Xcode template)

---
+++ watch-calculator/watch-calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  watch-calculator
+//
+//  Created by Hampus Nilsson on 14/10/15.
+//  Copyright © 2015 Hampus Nilsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
